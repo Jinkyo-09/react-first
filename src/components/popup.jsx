858b0437b9f57804779db8fd6b3cac37f4d65ed5
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-26 09:45:32 +0900

    팝업 안에 버튼으로 숫자 증가 이벤트 연결

--- src/components/popup.jsx
+++ src/components/popup.jsx
@@ -1,5 +1,26 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 function Popup() {
-	return <aside>popup</aside>;
+	const [num, setnum] = useState(0);
+
+	useEffect(() => {
+		//컴포넌트 mount시 한번만 실행
+		console.log('팝업 생성');
+
+		return () => {
+			//컴포넌트 unmount시 한번만 실행
+			console.log('팝업 소멸');
+		};
+	}, []);
+
+	return (
+		<aside>
+			<h1>{num}</h1>
+			<button onClick={() => setnum(num - 1)}>minus</button>
+			<button onClick={() => setnum(num + 1)}>plus</button>
+		</aside>
+	);
 }
 
 export default Popup;
