fa40fa19f0295266d12dc1de610d5c1fddd03e6f
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-26 10:43:43 +0900

    useRef를 활용한 불핑료한 재랜더링 방지 및 모션 작업

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react';
-import List from './components/List';
-import Footer from './components/footer';
-import Header from './components/header.jsx';
-import Popup from './components/popup';
+import { useRef, useState } from 'react';
 import './style.scss';
 
 function App() {
 	console.log('re-render');
-	const [Num, setNum] = useState(0);
+	//회전값에 적용할 증가하거나 감소할 숫자값을 Num이라는 빈 참조 객체에 저장 (해당값이 바뀌더라도 컴포넌트를 재랜더링하지 않기 위함)
+	let Num = useRef(0);
+	//가상돔인 아티클 요소를 담기위한 빈 참조객체 생성
+	//아직 실제적으로 만들어지지 않은 돔을 임의로 담아서 이벤트 호출할때 불러오기 위함
+	let Box = useRef(null);
+	console.log(Num);
+
+	const prev = () => {
+		//Box.current에는 아직 생성되지 않은 가상돔 상태인 아티클을 가져와서 NUm값이 변경될 때마다 style 적용
+		//이때 num값 자체가 state가 아닌 useRef로 담아놓은 값이기 때문에 해당값이 변경되더라도 컴포넌트가 재랜더링 되지 않음
+		Box.current.style.transform = `rotate(${--Num.current * 45}deg)`;
+	};
+	const next = () => {
+		Box.current.style.transform = `rotate(${++Num.current * 45}deg)`;
+	};
 
 	return (
 		<>
-			<button onClick={() => setNum(Num - 1)}>PREV</button>
-			<button onClick={() => setNum(Num + 1)}>NEXT</button>
+			<button onClick={prev}>PREV</button>
+			<button onClick={next}>NEXT</button>
 
-			<article style={{ transform: `rotate(${45 * Num}deg)` }}></article>
+			{/* 참조객체에 가상돔 요소를 연결할 때에는 ref={참조객체명} */}
+			<article ref={Box}></article>
 		</>
 	);
 }
