f029e2ff18228b8d0f68d104fc79c40d3b9fcb06
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-26 09:51:19 +0900

    useEffect를 통한 컴포넌트의 생명주기 설명

--- src/components/popup.jsx
+++ src/components/popup.jsx
@@ -1,5 +1,10 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
+//useEffect : 컴포넌트의 생성, 변화, 소멸의 특정 상태마다 원하느느 이벤트 연결 가능
+//useEffect(실행할 함수, 의존성 배열)
+//의존성배열이 비어있는 상태에서 함수 안쪽에 구문을 입력하면 컴포넌트 마운트시 한번만 실행
+//의존성배열이 비어있는 상태에서 리턴되는 함수 안쪽에 구문을 입력하면 컴포넌트 언마운트시 한번만 실행
+//의존성배열에 특정 스테이트를 담아두고 함수 안쪽에 구문을 입력하면 해당 스테이트값이 변경될때마다 실행
 
 function Popup() {
 	const [num, setnum] = useState(0);
